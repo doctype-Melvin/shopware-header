71fddc9f7a8958b4c29bc9946fa576a7d5268863
doctype-Melvin <doctype.melvin@gmail.com>
2023-08-16 10:41:26 +0200

    initial commit

---
+++ src/MyHeader.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace MyHeader;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class MyHeader extends Plugin implements ThemeInterface
+{
+}
